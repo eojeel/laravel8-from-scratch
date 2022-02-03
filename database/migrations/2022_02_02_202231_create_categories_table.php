92b19e774e0b68e638dd83952d2d51efbb76878c
Joe Lee <joe@joelee.io>
2022-02-03 20:43:14 +0000

    #004 Database Seeding Saves Time

--- database/migrations/2022_02_02_202231_create_categories_table.php
+++ database/migrations/2022_02_02_202231_create_categories_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,8 +17,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
 
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+
         // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'Personal',
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'Family',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'Work',
+        ]);
+
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'Title' => 'First Post',
+            'Slug' => 'first-post',
+            'Excerpt' => '<p>This is the third post</p>',
+            'Body' => '<p>Proin molestie odio felis, id vulputate tortor tristique vel. Sed in metus eget enim blandit efficitur. Nam auctor lorem ut mollis vestibulum. Nunc interdum venenatis mollis. Proin at urna eleifend, varius lacus eu, pulvinar quam. Donec eget molestie libero. Aliquam tincidunt lorem ultricies, aliquet dolor quis, pharetra metus. Aliquam varius convallis nulla, nec molestie eros mattis sit amet. Phasellus metus nisl, imperdiet et vehicula in, vehicula id elit. Vestibulum maximus mauris quis lorem vehicula, non tristique magna varius. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis in pellentesque urna. Fusce at ex augue. Etiam maximus sollicitudin mattis. Cras sodales lacinia mi, bibendum fringilla leo euismod vitae.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'Title' => 'second Post',
+            'Slug' => 'second-post',
+            'Excerpt' => '<p>This is the third post</p>',
+            'Body' => '<p>Proin molestie odio felis, id vulputate tortor tristique vel. Sed in metus eget enim blandit efficitur. Nam auctor lorem ut mollis vestibulum. Nunc interdum venenatis mollis. Proin at urna eleifend, varius lacus eu, pulvinar quam. Donec eget molestie libero. Aliquam tincidunt lorem ultricies, aliquet dolor quis, pharetra metus. Aliquam varius convallis nulla, nec molestie eros mattis sit amet. Phasellus metus nisl, imperdiet et vehicula in, vehicula id elit. Vestibulum maximus mauris quis lorem vehicula, non tristique magna varius. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis in pellentesque urna. Fusce at ex augue. Etiam maximus sollicitudin mattis. Cras sodales lacinia mi, bibendum fringilla leo euismod vitae.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'Title' => 'third Post',
+            'Slug' => 'third-post',
+            'Excerpt' => '<p>This is the third post</p>',
+            'Body' => '<p>Proin molestie odio felis, id vulputate tortor tristique vel. Sed in metus eget enim blandit efficitur. Nam auctor lorem ut mollis vestibulum. Nunc interdum venenatis mollis. Proin at urna eleifend, varius lacus eu, pulvinar quam. Donec eget molestie libero. Aliquam tincidunt lorem ultricies, aliquet dolor quis, pharetra metus. Aliquam varius convallis nulla, nec molestie eros mattis sit amet. Phasellus metus nisl, imperdiet et vehicula in, vehicula id elit. Vestibulum maximus mauris quis lorem vehicula, non tristique magna varius. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis in pellentesque urna. Fusce at ex augue. Etiam maximus sollicitudin mattis. Cras sodales lacinia mi, bibendum fringilla leo euismod vitae.</p>'
+        ]);
     }
 }
