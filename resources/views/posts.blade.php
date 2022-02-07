1bcd34f6bca3f2615add1306f85f4d9bc739a4d9
Joe Lee <joe@joelee.io>
2022-02-07 20:10:14 +0000

    #004 View All Posts By An Author

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,9 +4,11 @@
 
 <article class="{{ $loop->even ? 'mb-4' : '' }}">
 
-    <a href="/post/{{ $post->slug }}">
+    <a href="/post/{{ $post->slug }}"><h1>{!! $post->title !!}</h1></a>
 
-    {!! $post->title !!}</a>
+    <p>
+        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+       </p>
 
     <div>{{ $post->excerpt }}</div>
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 /*
@@ -28,7 +29,7 @@ Route::get('/', function () {
 
 
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest('created_at')->with('category', 'author')->get()
     ]);
 });
 
@@ -45,3 +46,10 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'posts' => $category->posts
     ]);
 });
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
+    ]);
+});
+
