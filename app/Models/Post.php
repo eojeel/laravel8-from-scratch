4e004ca1b28f39ba9c7c4cf31046cb9b0e7edaba
Joe Lee <joe@joelee.io>
2022-02-11 19:38:49 +0000

    #007 Fix a Confusing Eloquent Query Bug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,18 +16,19 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-        $query->when($filters['search'] ?? false, function($query, $search)
-        {
-            $query
-            ->where('title', 'like', '%'. $search .'%')
-            ->orWhere('body', 'like', '%'. $search .'%');
-        });
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('body', 'like', '%' . $search . '%')
+            )
+        );
 
-        $query->when($filters['category'] ?? false, function($query, $category)
-        {
-            $query->whereHas('category', fn($query) =>
-            $query->where('slug', $category));
-        });
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn ($query) =>
+                $query->where('slug', $category)
+            )
+        );
 
                     /*
             $query
@@ -38,11 +39,11 @@ class Post extends Model
             );
             */
 
-        $query->when($filters['author'] ?? false, function($query, $author)
-        {
-            $query->whereHas('author', fn($query) =>
-            $query->where('username', $author));
-        });
+            $query->when($filters['author'] ?? false, fn($query, $author) =>
+            $query->whereHas('author', fn ($query) =>
+                $query->where('username', $author)
+            )
+        );
 
     }
 
