4bc1e0bbb8d1f504e70f0c390350122bee85527a
Joe Lee <joe@joelee.io>
2022-02-11 19:25:32 +0000

    #007 Merge Category and Search Queries

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,26 +1,5 @@
 
 <x-layout>
-<?php /*
-@foreach($posts as $post)
-
-<article class="{{ $loop->even ? 'mb-4' : '' }}">
-
-    <a href="/post/{{ $post->slug }}"><h1>{!! $post->title !!}</h1></a>
-
-    <p>
-        By <a href="?author={{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-       </p>
-
-    <div>{{ $post->excerpt }}</div>
-
-    <p>
-        <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-    </p>
-
-</article>
-    @endforeach
-*/ ?>
-
         @include('posts._header');
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
@@ -33,7 +12,5 @@
                 <p style="text-center">No posts yet</p>
             @endif
 
-
         </main>
-
 </x-layout>
