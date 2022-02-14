62caa679b11b69ebaf8092dffe0520e5677b67d0
Joe Lee <joe@joelee.io>
2022-02-14 07:23:59 +0000

    #010 Table Consistency and Foreign Key Constraints

---
+++ database/migrations/2022_02_14_071318_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('posts_id')->references('id')->on('posts')->cascadeOnDelete();
+            $table->unsignedBigInteger('user_id');
+            $table->text('body');
+            $table->timestamps();
+            //$table->forign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
