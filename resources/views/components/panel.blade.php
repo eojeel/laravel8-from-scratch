875de3e8be6c55b026ca2cd307ce05d4145bdb2c
Joe Lee <joe@joelee.io>
2022-02-15 09:43:52 +0000

    #010 Design the Comment Form

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes(['class' => 'order border-gray-200 p-6 rounded-xl']) }}>
+    {{ $slot }}
+</div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -70,6 +70,26 @@
 
 
         <section class="col-span-8 col-start-5 m-10 space-y-4">
+            <x-panel>
+                <form method="POST" action="/">
+                    @csrf
+                    <header class="flex items-center">
+                        <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="60" height="60" class="rounded-full">
+                        <h2 class="ml-4">Want to Participate?</h2>
+                        </header>
+
+                        <div class="mt-6">
+                            <textarea name="body" class="w-full text-sm focus::outline-none focus:ring" cols="30" rows="5" placeholder="Comment!"></textarea>
+                        </div>
+
+                        <div class="flex justify-end mt-8 pt-6 border-t border-gray-200">
+                            <button type="submit" class="bg-blue-500 text-white font-semibold text-xs py-2 px-10 hover:bg-blue-700 rounded-xl">Post</button>
+                        </div>
+                </form>
+            </x-panel>
+        </section>
+
+        <section class="col-span-8 col-start-5 m-10 space-y-4">
             @foreach ($post->comments as $comment)
             <x-post-comment :comment="$comment"/>
             @endforeach
